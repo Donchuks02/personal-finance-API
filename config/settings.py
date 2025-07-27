f86622b82b6659af6312e05f8cdcc54c8b86d1b8
donchuks <chukwudidavid02@gmail.com>
2025-07-27 11:29:23 +0100

    Created and configured all the apps needed

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Third-party apps
+    "rest_framework",
+
+    # Local apps
+    "users.apps.UsersConfig",
+    "reports.apps.ReportsConfig",
+    "transactions.apps.TransactionsConfig",
+    "budgets.apps.BudgetsConfig",
+
 ]
 
 MIDDLEWARE = [
