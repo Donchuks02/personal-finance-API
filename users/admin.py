27452a17546687364e7051a55db844e75cb63551
donchuks <chukwudidavid02@gmail.com>
2025-07-29 17:17:41 +0100

    created users model in the users app and register it in the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'date_joined')
+    fieldsets = (
+        (None, {'fields': ('email', 'name', 'password')}),
+        ('personal info', {'fields': ('name',)}),
+        ('permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'is_staff', 'is_active')
+        })
+    )
+
+    search_fields = ('email', 'name',)
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils import timezone
 
-# Create your models here.
+# Create your models here. 
+
+
+# This class handles the creation of user, the first method creates a regular user while the second method creates a superuser(admin).
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        # Ensure that email is provided
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email) # make the provided email lowercase
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        # ensure that superuser has staff access and superuser privileges
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        # Reuse the create_user method attributes to create a superuser
+        return self.create_user(email, password, **extra_fields)
+
+
+# This class defines the custom user model that will be used in the application, this replaces the default user model that django provides.
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    # This is constant defines which field will be used as a unique identifier for authentication
+    USERNAME_FIELD = 'email'
+
+    # the required field that must be provided when creating a superuser aside from the email.
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
