cdec7f4b2fec1f4c69ef73dab4f4c0e97b260894
donchuks <chukwudidavid02@gmail.com>
2025-08-06 12:10:33 +0100

    modified the model for the transaction app, added a predefined category

--- transactions/models.py
+++ transactions/models.py
@@ -8,15 +8,40 @@ class Transaction(models.Model):
 
     INCOME = "IN"
     EXPENSE = "EX"
+
     TRANSACTION_TYPES =[
         (INCOME, "Income"),
         (EXPENSE, "Expense"),
     ]
 
+    CATEGORY_CHOICES = [
+        ('food_groceries', 'Food & Groceries'),
+        ('transportation', 'Transportation'),
+        ('rent', 'Rent'),
+        ('utilities', 'Utilities'),
+        ('internet_phone', 'Internet & Phone'),
+        ('entertainment', 'Entertainment'),
+        ('health_medical', 'Health & Medical'),
+        ('clothing', 'Clothing'),
+        ('education', 'Education'),
+        ('savings', 'Savings'),
+        ('gifts_donations', 'Gifts & Donations'),
+        ('personal_care', 'Personal Care'),
+        ('travel', 'Travel'),
+        ('home_maintenance', 'Home Maintenance'),
+        ('childcare', 'Childcare'),
+        ('insurance', 'Insurance'),
+        ('salary', 'Salary'),
+        ('investment_income', 'Investment Income'),
+        ('other', 'Other'),
+    ]
+
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="transactions")
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     transaction_type = models.CharField(max_length=2, choices=TRANSACTION_TYPES)
-    category = models.CharField(max_length=100)
+    # category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, blank=True, null=True)
     description = models.TextField(blank=True)
     date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
